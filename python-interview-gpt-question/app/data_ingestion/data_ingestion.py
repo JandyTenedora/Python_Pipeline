0140ef9ffc9446fac5bafb8a939b23427f4959c0
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-01-23 19:44:02 +0000

    refactor(pigq/data_ingestion.py) create data ingestion class

--- python-interview-gpt-question/app/data_ingestion/data_ingestion.py
+++ python-interview-gpt-question/app/data_ingestion/data_ingestion.py
@@ -10,27 +10,35 @@ from app.utils.find_project_root import find_project_root
 project_root = str(find_project_root())
 sys.path.append(project_root)
 
-def main():
-   ## Possible new pattern to ingest without the need of io write
-   # normalised_customer_activity, product_lookup = normalise_customer_activity()
+class DataIngestor:
+   def __init__(self, project_root, bq_dataset_id, firestore_dataset_name):
+      self.project_root = project_root
+      self.bq_dataset_id = bq_dataset_id
+      self.firestore_dataset_name = firestore_dataset_name
 
+   def ingest_data(self):
+      # Create all bq and fs destinations
+      bq_sales_data = BigQueryDestination(self.bq_dataset_id, "raw_sales_data")
+      fs_event_logs = FirestoreDestination(self.firestore_dataset_name, "event_log_raw")
+      bq_customer_activity = BigQueryDestination(self.bq_dataset_id, "customer_logs")
+
+      # Create necessary ingestors
+      sales_csv_ingestor = CSVDataIngestor(f"{self.project_root}/target/raw/large_sales_data.csv", bq_sales_data)
+      event_json_ingestor = JSONIngestor(f"{self.project_root}/target/raw/large_event_logs.json", fs_event_logs)
+      customer_activity_json_ingestor = JSONIngestor(f"{self.project_root}/target/normalized/normalized_customer_logs.json", bq_customer_activity)
+
+      # Run ingestion
+      sales_csv_ingestor.ingest_data()
+      event_json_ingestor.ingest_data()
+      customer_activity_json_ingestor.ingest_data()
+
+
+def main():
    dataset_id = "ecommerce_data_raw"
    firestore_dataset_name = "ecommerce-firestore"
-   bq_sales_data = BigQueryDestination(dataset_id, "raw_sales_data")
-   bq_event_logs = FirestoreDestination(firestore_dataset_name, "event_log_raw")
-   bq_customer_activity = BigQueryDestination(dataset_id, "customer_logs")
-   bq_products = FirestoreDestination(firestore_dataset_name, "product_catalog")
+   data_ingestor = DataIngestor(project_root, dataset_id, firestore_dataset_name)
+   data_ingestor.ingest_data()
 
-   normalise_customer_activity_write()
-   sales_csv_ingestor = CSVDataIngestor(f"{project_root}/target/raw/large_sales_data.csv", bq_sales_data)
-   event_json_ingestor = JSONIngestor(f"{project_root}/target/raw/large_event_logs.json", bq_event_logs)
-   customer_activity_json_ingestor = JSONIngestor(f"{project_root}/target/normalized/normalized_customer_logs.json", bq_customer_activity)
-   products_json_ingestor = JSONIngestor(f"{project_root}/target/normalized/product_lookup.json", bq_products)
-
-   sales_csv_ingestor.ingest_data()
-   event_json_ingestor.ingest_data()
-   customer_activity_json_ingestor.ingest_data()
-   products_json_ingestor.ingest_data()
 
 if __name__ == "__main__":
     main()
